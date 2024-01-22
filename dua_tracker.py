e765c287e378e2095de171a1a21496504e505bcd
Dillon Shearer <dillshearer@outlook.com>
2024-01-22 13:18:59 -0500

    remove individuals from data pull

--- dua_tracker.py
+++ dua_tracker.py
@@ -220,7 +220,7 @@ def update_tracker(users_fp_raw):
     individual_ids = df5['ID'].tolist()
 
     # Drop rows in new_users where 'ID' is in either of the two lists
-    new_users = new_users[~new_users['ID'].isin(completed_ids + individual_ids)]
+    new_users = new_users[~new_users['ID'].isin(completed_ids + completed_individual_ids)]
 
     # Filtering the DataFrame
     df_filtered = df_new[~df_new['Id'].isin(new_users)]
