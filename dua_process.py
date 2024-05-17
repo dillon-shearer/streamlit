baf53ac10be70516cdc386115f6e29145ea27061
DILLON SHEARER <dillshearer@outlook.com>
2024-05-17 16:49:21 -0400

    push updates - new buckets, updated visual, updated legend

--- dua_process.py
+++ dua_process.py
@@ -27,12 +27,12 @@ def show():
             .legend-item {
                 display: flex;
                 align-items: center;
-                margin-right: 20px; /* Space between items */
+                margin-right: 20px;
             }
             .legend-box {
                 width: 20px;
                 height: 20px;
-                margin-right: 5px; /* Space between box and label */
+                margin-right: 5px;
             }
             .legend-label {
                 font-size: 16px;
@@ -45,15 +45,27 @@ def show():
             </div>
             <div class="legend-item">
                 <div class="legend-box" style="background-color: rgb(114, 44, 121);"></div>
-                <span class="legend-label">Tracker to DS</span>
+                <span class="legend-label">Data Verification</span>
             </div>
             <div class="legend-item">
                 <div class="legend-box" style="background-color: rgb(198, 47, 105);"></div>
-                <span class="legend-label">DS to Final User Sign</span>
+                <span class="legend-label">Verified to DAC Send</span>
             </div>
             <div class="legend-item">
                 <div class="legend-box" style="background-color: rgb(58, 149, 136);"></div>
-                <span class="legend-label">Final User to Foundation</span>
+                <span class="legend-label">DAC Turn-Around</span>
+            </div>
+            <div class="legend-item">
+                <div class="legend-box" style="background-color: rgb(255, 165, 0);"></div>
+                <span class="legend-label">Send DocuSign</span>
+            </div>
+            <div class="legend-item">
+                <div class="legend-box" style="background-color: rgb(75, 0, 130);"></div>
+                <span class="legend-label">User DocuSign</span>
+            </div>
+            <div class="legend-item">
+                <div class="legend-box" style="background-color: rgb(220, 20, 60);"></div>
+                <span class="legend-label">Foundation Signature</span>
             </div>
         </div>
         """
@@ -61,32 +73,46 @@ def show():
 
     # Function to prepare tasks for the Gantt chart
     def prepare_gantt_chart_data(df):
+        # Strip any leading or trailing spaces from column names
+        df.columns = df.columns.str.strip()
+
         # Handle non-date strings and convert dates
-        df['Date Requested'] = pd.to_datetime(df['Date Requested'], errors='coerce')
-        df['Date Added to Tracker'] = pd.to_datetime(df['Date Added to Tracker'], errors='coerce')
-        df['DS Send Date'] = pd.to_datetime(df['DS Send Date'], errors='coerce')
-        df['Last Collaborator Sign Date'] = pd.to_datetime(df['Last Collaborator Sign Date'], errors='coerce')
-        df['Foundation Sign Date'] = pd.to_datetime(df['Foundation Sign Date'], errors='coerce')
+        date_columns = [
+            'Date Requested', 'Date Added to Tracker', 'Data Verified Date', 'DAC Send Date', 
+            'DAC Approval Date', 'DS Send Date', 'Requestor Sign Date', 'BO Sign Date', 
+            'Last Collaborator Sign Date', 'Foundation Sign Date'
+        ]
+
+        for col in date_columns:
+            if col not in df.columns:
+                st.error(f"Missing column: {col}")
+                return None
+
+            df[col] = pd.to_datetime(df[col], errors='coerce')
+
+        # Calculate the last sign date
+        df['Last Sign Date'] = df[['Requestor Sign Date', 'BO Sign Date', 'Last Collaborator Sign Date']].max(axis=1)
 
         tasks = []
         for index, row in df.iterrows():
             request_id = f"Request {index + 1}"
-            start = row['Date Requested']
-            finish = adjust_finish_time(start, row['Date Added to Tracker'])
-            tasks.append(dict(Task=request_id, Start=start, Finish=finish, Resource='Req to Tracker'))
+            phases = [
+                ('Req to Tracker', row['Date Requested'], row['Date Added to Tracker']),
+                ('Data Verification', row['Date Added to Tracker'], row['Data Verified Date']),
+                ('Verified to DAC Send', row['Data Verified Date'], row['DAC Send Date']),
+                ('DAC Turn-Around', row['DAC Send Date'], row['DAC Approval Date']),
+                ('Send DocuSign', row['DAC Approval Date'], row['DS Send Date']),
+                ('User DocuSign', row['DS Send Date'], row['Last Sign Date']),
+                ('Foundation Signature', row['Last Sign Date'], row['Foundation Sign Date'])
+            ]
 
-            start = row['Date Added to Tracker']
-            finish = adjust_finish_time(start, row['DS Send Date'])
-            tasks.append(dict(Task=request_id, Start=start, Finish=finish, Resource='Tracker to DS'))
-
-            if pd.notna(row['Last Collaborator Sign Date']):
-                start = row['DS Send Date']
-                finish = adjust_finish_time(start, row['Last Collaborator Sign Date'])
-                tasks.append(dict(Task=request_id, Start=start, Finish=finish, Resource='DS to Final User Sign'))
-
-            start = row['Last Collaborator Sign Date'] if pd.notna(row['Last Collaborator Sign Date']) else row['DS Send Date']
-            finish = adjust_finish_time(start, row['Foundation Sign Date'])
-            tasks.append(dict(Task=request_id, Start=start, Finish=finish, Resource='Final User to Foundation'))
+            for phase in phases:
+                resource, start, finish = phase
+                start = pd.to_datetime(start, errors='coerce')
+                finish = pd.to_datetime(finish, errors='coerce')
+                if pd.notna(start) and pd.notna(finish):
+                    finish = adjust_finish_time(start, finish)
+                    tasks.append(dict(Task=request_id, Start=start, Finish=finish, Resource=resource))
 
         return tasks
 
@@ -94,124 +120,107 @@ def show():
     def create_gantt_chart(tasks):
         colors = {
             'Req to Tracker': 'rgb(46, 137, 205)',
-            'Tracker to DS': 'rgb(114, 44, 121)',
-            'DS to Final User Sign': 'rgb(198, 47, 105)',
-            'Final User to Foundation': 'rgb(58, 149, 136)'
+            'Data Verification': 'rgb(114, 44, 121)',
+            'Verified to DAC Send': 'rgb(198, 47, 105)',
+            'DAC Turn-Around': 'rgb(58, 149, 136)',
+            'Send DocuSign': 'rgb(255, 165, 0)',
+            'User DocuSign': 'rgb(75, 0, 130)',
+            'Foundation Signature': 'rgb(220, 20, 60)'
         }
         fig = ff.create_gantt(tasks, colors=colors, index_col='Resource', show_colorbar=False, group_tasks=True)
         fig.update_layout(showlegend=False, yaxis=dict(showticklabels=False))
         return fig
     
     def calculate_durations(df):
+        # Ensure all date columns are datetime and handle NaT by filling with previous valid date
+        date_columns = [
+            'Date Requested', 'Date Added to Tracker', 'Data Verified Date', 'DAC Send Date', 
+            'DAC Approval Date', 'DS Send Date', 'Requestor Sign Date', 'BO Sign Date', 
+            'Last Collaborator Sign Date', 'Foundation Sign Date'
+        ]
+
+        for col in date_columns:
+            df[col] = pd.to_datetime(df[col], errors='coerce')
+
+        # Calculate the last sign date
+        df['Last Sign Date'] = df[['Requestor Sign Date', 'BO Sign Date', 'Last Collaborator Sign Date']].max(axis=1)
+
         # Calculate durations in days
         df['Req to Tracker Duration'] = (df['Date Added to Tracker'] - df['Date Requested']).dt.days
-        df['Tracker to DS Duration'] = (df['DS Send Date'] - df['Date Added to Tracker']).dt.days
-        df['DS to Final User Sign Duration'] = (df['Last Collaborator Sign Date'] - df['DS Send Date']).dt.days
-        df['Final User to Foundation Duration'] = (df['Foundation Sign Date'] - df['Last Collaborator Sign Date']).dt.days
+        df['Data Verification Duration'] = (df['Data Verified Date'] - df['Date Added to Tracker']).dt.days
+        df['Verified to DAC Send Duration'] = (df['DAC Send Date'] - df['Data Verified Date']).dt.days
+        df['DAC Turn-Around Duration'] = (df['DAC Approval Date'] - df['DAC Send Date']).dt.days
+        df['Send DocuSign Duration'] = (df['DS Send Date'] - df['DAC Approval Date']).dt.days
+        df['User DocuSign Duration'] = (df['Last Sign Date'] - df['DS Send Date']).dt.days
+        df['Foundation Signature Duration'] = (df['Foundation Sign Date'] - df['Last Sign Date']).dt.days
 
         # Calculate aggregate metrics
         durations_summary = {
             'Req to Tracker': df['Req to Tracker Duration'].sum(),
-            'Tracker to DS': df['Tracker to DS Duration'].sum(),
-            'DS to Final User Sign': df['DS to Final User Sign Duration'].sum(),
-            'Final User to Foundation': df['Final User to Foundation Duration'].sum()
+            'Data Verification': df['Data Verification Duration'].sum(),
+            'Verified to DAC Send': df['Verified to DAC Send Duration'].sum(),
+            'DAC Turn-Around': df['DAC Turn-Around Duration'].sum(),
+            'Send DocuSign': df['Send DocuSign Duration'].sum(),
+            'User DocuSign': df['User DocuSign Duration'].sum(),
+            'Foundation Signature': df['Foundation Signature Duration'].sum()
         }
         return durations_summary
 
     def calculate_average_durations(df):
+        # Ensure all date columns are datetime and handle NaT by filling with previous valid date
+        date_columns = [
+            'Date Requested', 'Date Added to Tracker', 'Data Verified Date', 'DAC Send Date', 
+            'DAC Approval Date', 'DS Send Date', 'Requestor Sign Date', 'BO Sign Date', 
+            'Last Collaborator Sign Date', 'Foundation Sign Date'
+        ]
+
+        for col in date_columns:
+            df[col] = pd.to_datetime(df[col], errors='coerce')
+
+        # Calculate the last sign date
+        df['Last Sign Date'] = df[['Requestor Sign Date', 'BO Sign Date', 'Last Collaborator Sign Date']].max(axis=1)
+
+        # Calculate durations in days
+        df['Req to Tracker Duration'] = (df['Date Added to Tracker'] - df['Date Requested']).dt.days
+        df['Data Verification Duration'] = (df['Data Verified Date'] - df['Date Added to Tracker']).dt.days
+        df['Verified to DAC Send Duration'] = (df['DAC Send Date'] - df['Data Verified Date']).dt.days
+        df['DAC Turn-Around Duration'] = (df['DAC Approval Date'] - df['DAC Send Date']).dt.days
+        df['Send DocuSign Duration'] = (df['DS Send Date'] - df['DAC Approval Date']).dt.days
+        df['User DocuSign Duration'] = (df['Last Sign Date'] - df['DS Send Date']).dt.days
+        df['Foundation Signature Duration'] = (df['Foundation Sign Date'] - df['Last Sign Date']).dt.days
+
         # Calculate average durations
         average_durations = {
             'Req to Tracker': df['Req to Tracker Duration'].mean(),
-            'Tracker to DS': df['Tracker to DS Duration'].mean(),
-            'DS to Final User Sign': df['DS to Final User Sign Duration'].mean(),
-            'Final User to Foundation': df['Final User to Foundation Duration'].mean()
+            'Data Verification': df['Data Verification Duration'].mean(),
+            'Verified to DAC Send': df['Verified to DAC Send Duration'].mean(),
+            'DAC Turn-Around': df['DAC Turn-Around Duration'].mean(),
+            'Send DocuSign': df['Send DocuSign Duration'].mean(),
+            'User DocuSign': df['User DocuSign Duration'].mean(),
+            'Foundation Signature': df['Foundation Signature Duration'].mean()
         }
         return average_durations
-    
-    # Function to display pie chart without internal legend
+
+    # Function to display pie chart with average durations
     def display_pie_chart(durations):
         labels = list(durations.keys())
         values = list(durations.values())
-        colors = ['rgb(46, 137, 205)', 'rgb(114, 44, 121)', 'rgb(198, 47, 105)', 'rgb(58, 149, 136)']
-        fig = px.pie(names=labels, values=values, title="Total Days Spent in Each Phase", hole=0.3)
-        fig.update_traces(textinfo='label+value', marker=dict(colors=colors))
+        colors = ['rgb(46, 137, 205)', 'rgb(114, 44, 121)', 'rgb(198, 47, 105)', 'rgb(58, 149, 136)',
+                  'rgb(255, 165, 0)', 'rgb(75, 0, 130)', 'rgb(220, 20, 60)']
+        fig = px.pie(names=labels, values=values, title="Average Days Spent in Each Phase", hole=0.3)
+        fig.update_traces(texttemplate='%{label}: %{value:.2f}', textinfo='label+value', marker=dict(colors=colors))
         fig.update_layout(showlegend=False, autosize=False, width=1250, height=750)
         st.plotly_chart(fig, use_container_width=True)
-        
-    def display_colored_metrics(durations, label_prefix):
-        # Define CSS styles and container layout
-        metric_html = """
-        <style>
-            .metrics-container {{
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                flex-wrap: wrap;
-                margin: 0 auto;
-                width: 100%;
-            }}
-            .metric-box {{
-                text-align: center;
-                padding: 10px;
-                border-radius: 0;
-                color: white;
-                background-color: inherit;  /* Inherits background-color from style attribute */
-                flex-grow: 1;
-                min-width: 100px;
-                height: 120px;
-                margin: 1px;
-            }}
-            .metric-value {{
-                font-size: 25px;
-                margin: 2px 0;
-            }}
-            .metric-label {{
-                font-size: 16px;
-                margin: 0;
-            }}
-        </style>
-        <div class="metrics-container">
-            {metrics}
-        </div>
-        """
-
-        # Individual metric box format
-        individual_metric_html = """
-        <div class="metric-box" style="background-color: {color};">
-            <h2 class="metric-value">{value:.1f}</h2>
-            <h4 class="metric-label">{label}</h4>
-        </div>
-        """
-
-        # Colors for each phase
-        colors = {
-            'Req to Tracker': 'rgb(46, 137, 205)',
-            'Tracker to DS': 'rgb(114, 44, 121)',
-            'DS to Final User Sign': 'rgb(198, 47, 105)',
-            'Final User to Foundation': 'rgb(58, 149, 136)'
-        }
-
-        # Generating individual metrics
-        metrics = ''.join([
-            individual_metric_html.format(color=colors[key], value=durations[key], label=key + " " + label_prefix)
-            for key in durations
-        ])
-
-        # Rendering the full metrics container with all boxes
-        st.markdown(metric_html.format(metrics=metrics), unsafe_allow_html=True)
-
 
     # File uploader and chart logic
     data_file = st.file_uploader("Upload your data file in Excel format:", type=["xlsx"])
     if data_file is not None:
         df = pd.read_excel(data_file)
         tasks = prepare_gantt_chart_data(df)
-        gantt_chart_fig = create_gantt_chart(tasks)
-        display_custom_legend()  # Display custom legend
-        st.plotly_chart(gantt_chart_fig, use_container_width=True)
+        if tasks:
+            gantt_chart_fig = create_gantt_chart(tasks)
+            display_custom_legend()  # Display custom legend
+            st.plotly_chart(gantt_chart_fig, use_container_width=True)
 
-        total_durations = calculate_durations(df)
-        display_pie_chart(total_durations)
-
-        average_durations = calculate_average_durations(df)
-        display_colored_metrics(average_durations, "Avg Days")
+            average_durations = calculate_average_durations(df)
+            display_pie_chart(average_durations)
